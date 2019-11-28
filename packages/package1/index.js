35240b46772e994ff2cc5b9101f581a42281277c
volodymyr.mazur <vovamazur3@gmail.com>
2019-11-28 15:44:45 +0200

    update new verions

--- packages/package1/index.js
+++ packages/package1/index.js
@@ -1,4 +1,4 @@
 module.exports = () => {
     console.log('package1')
-    console.log('version N')
+    console.log('version 1')
 }
--- packages/package2/index.js
+++ packages/package2/index.js
@@ -1,4 +1,4 @@
 module.exports = () => {
     console.log('package2')
-    console.log('version N')
+    console.log('version 1')
 }
